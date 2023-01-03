8160d1c7604f65e38575b6a84fa8f6a234a7e23c
hdy86 <dyhong86@naver.com>
2023-01-03 14:06:59 +0900

    BrowserRouter to createBrowserRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import { createBrowserRouter } from "react-router-dom";
 import About from "./screens/About";
 import Home from "./screens/Home";
+import Root from "./Root";
+import NotFound from "./screens/NotFound";
+import ErrorComponent from "./components/ErrorComponent";
 
-function Router() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+        errorElement: <ErrorComponent />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+    errorElement: <NotFound />,
+  },
+]);
 
-export default Router;
+export default router;
